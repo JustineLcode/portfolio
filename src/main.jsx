fc95f56849d5e0111f19fdb590d26fb62cc14d07
JustineLcode <leulierjustine@hotmail.com>
2024-12-18 21:42:43 +0100

    18-12

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import './home.scss';/* Pour importer les fichiers Sass!! */
 import './about.scss';
 import './projects.scss';
+import './Programminglanguages.scss';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
